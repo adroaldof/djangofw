c08fbb3f111b489373b3848e3f413c2688881ebb
Adroaldo de Andrade <adroaldof@gmail.com>
2013-11-04 15:35:15 -0200

    Change was published recently representation

--- polls/models.py
+++ polls/models.py
@@ -1,18 +1,23 @@
 import datetime
 
-from django.utils import timezone
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
 
 
 class Poll(models.Model):
     question = models.CharField(max_length=256)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField(_('Date published'))
 
     def __unicode__(self):
         return self.question
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = _('Published recently')
 
 
 class Choice(models.Model):
