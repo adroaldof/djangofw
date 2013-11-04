df77b14a555013df6f68f46c224a2f5601d618b8
Adroaldo de Andrade <adroaldof@gmail.com>
2013-11-04 12:36:30 -0200

    Insert Poll and Choice classes to polls models

--- polls/models.py
+++ polls/models.py
@@ -1,1 +1,24 @@
+import datetime
+
+from django.utils import timezone
 from django.db import models
+
+
+class Poll(models.Model):
+    question = models.CharField(max_length=256)
+    pub_date = models.DateTimeField('date published')
+
+    def __unicode__(self):
+        return self.question
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice_text = models.CharField(max_length=256)
+    votes = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.choice_text
