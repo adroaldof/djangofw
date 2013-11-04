8c8eea52b082231387bbf58ffaf37a1ecefdfb1f
Adroaldo de Andrade <adroaldof@gmail.com>
2013-11-04 14:10:30 -0200

    Include polls app reference on routes

--- djangofw/urls.py
+++ djangofw/urls.py
@@ -8,6 +8,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'djangofw.views.home', name='home'),
+    url(r'^polls/', include('polls.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
