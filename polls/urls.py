f8aeb1b6ee88784454e79bc25ecd1aba9c5d3ff6
Adroaldo de Andrade <adroaldof@gmail.com>
2013-11-05 00:44:38 -0200

    Show latests 15 on polls home

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('polls.views',
+    # localhost:8000/polls
     url(r'^$', 'home', name='polls-home'),
 )
--- polls/views.py
+++ polls/views.py
@@ -2,4 +2,9 @@ from django.template.response import TemplateResponse
 
 
 def home(request):
-    return TemplateResponse(request, 'polls/index.html', {})
+    latests_poll_list = Poll.objects.order_by('-pub_date')[:15]
+    return TemplateResponse(request, 'polls/index.html', {
+        'latests_poll_list': latests_poll_list,
+    })
+
+
