c4cbf098a1d41e279ffc4f8c3ac04df70dea9046
Adroaldo de Andrade <adroaldof@gmail.com>
2013-11-11 14:36:10 -0200

    Add category route

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('blog.views',
     url(r'^$', 'index', name='blog-home'),
     url(r'^post/(?P<slug>[^\.]+)', 'posts',  name='blog-post'),
+    url(r'^category/(?P<slug>[^\.]+)', 'categories', name='blog-category'),
 )
