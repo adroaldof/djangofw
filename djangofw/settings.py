5253a37064708038b9c7dd06377041cb8a232bc8
Adroaldo de Andrade <adroaldof@gmail.com>
2013-11-11 00:26:35 -0200

    Add blog to installed apps

--- djangofw/settings.py
+++ djangofw/settings.py
@@ -140,6 +140,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'polls',
+    'blog',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
