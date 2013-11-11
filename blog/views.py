b7724debee85b049d0d1c0959f2808e7cf75e0e6
Adroaldo de Andrade <adroaldof@gmail.com>
2013-11-11 14:31:52 -0200

    Add blog index view

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,14 @@
 # Create your views here.
+from django.template.response import TemplateResponse
+from .models import Blog, Category
+
+
+def index(request):
+    categories = Category.objects.all()
+    posts = Blog.objects.all()[:5]
+    return TemplateResponse(request, 'blog/index.html', {
+        'categories': categories,
+        'posts': posts,
+    })
+
+
